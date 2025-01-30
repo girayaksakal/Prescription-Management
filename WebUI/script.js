0faff284face19c667eb695367852151eed2381e
Giray Aksakal <102882253+girayaksakal@users.noreply.github.com>
2025-01-30 15:25:01 +0300

    Update script.js

--- WebUI/script.js
+++ WebUI/script.js
@@ -1,4 +1,4 @@
-const API_BASE = window.location.origin;
+const API_BASE = "https://prescriptionmanager.azurewebsites.net";
 
 async function getPrescription() {
     const id = document.getElementById('prescriptionId').value;
